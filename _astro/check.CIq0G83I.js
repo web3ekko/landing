c21f1d473775ed63d5c9b96407f98df070ec2c1a
Abraham Alaka <chokosabe@gmail.com>
2026-01-07 10:23:43 +0000

    Deploy landing page - 2026-01-07

---
+++ _astro/check.CIq0G83I.js
@@ -0,0 +1,6 @@
+import{c}from"./FadeIn.Cd_vse8V.js";/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
